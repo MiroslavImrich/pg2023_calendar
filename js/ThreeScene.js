8ea84e77fd7a534d287035a2fa0dd48248475561
Lukáš Brilla <lukas.brilla@student.tuke.sk>
2023-11-19 14:45:39 +0100

    kalendar

--- js/ThreeScene.js
+++ js/ThreeScene.js
@@ -28,6 +28,17 @@ function init() {
     addObjects();
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    var geometrySphere = new THREE.SphereGeometry( 100, 100, 100 );
+    var cubeTexture = new THREE.ImageUtils.loadTexture(
+        'texture/sky.jpg' );
+    var materialSphere = new THREE.MeshBasicMaterial( {
+        map: cubeTexture,
+        transparent: true,
+        side: THREE.DoubleSide} );
+    sphere = new THREE.Mesh( geometrySphere, materialSphere );
+    sphere.position.set(0, 0, 0);
+    scene.add( sphere );
 }
 
 function render() {
@@ -43,12 +54,18 @@ function addObjects() {
     // Create the triangular prism geometry
     var geometryPrism = new THREE.CylinderGeometry(1.4, 1.4, 3, 3);
 
-    // Create the material for the prism
-    var materialPrism = new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        side: THREE.DoubleSide,
-        // Adjust the opacity as needed
+    // Create the material for the outer faces
+    var materialOuter = new THREE.MeshBasicMaterial({
+        color: 0xdddddd,
+        side: THREE.DoubleSide
     });
+
+// Create the material for the inner faces
+    var materialInner = new THREE.MeshBasicMaterial({
+        visible: false
+    });
+
+
 
     // Assign UV coordinates to the geometry for proper texture mapping
     geometryPrism.faceVertexUvs[0] = [];
@@ -59,17 +76,20 @@ function addObjects() {
     ]);
 
     // Create the triangular prism mesh
-    var prismMesh = new THREE.Mesh(geometryPrism, materialPrism);
+    var prismMesh = new THREE.Mesh(geometryPrism, [materialOuter, materialInner]);
     prismMesh.position.set(0, 0.2, 0);
     prismMesh.rotation.x = -Math.PI / 2;
     group.add(prismMesh);
 
-    // Add calendar to the slanted face of the prism
-    // Add calendar to the slanted face of the prism
 
-    var calendar = createMonthlyCalendar(2023, 2);
-    positionCalendarOnFace(calendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.3, 0.8, -0.62);
+
+
+
+    var calendar = createMonthlyCalendar(2008,1 );
+    positionCalendarOnFace(calendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.33, 0.75, -0.62);
     group.add(calendar);
+
+
 
 
 
@@ -91,12 +111,12 @@ function positionCalendarOnFace(calendar, prism, rotationY, rotationX, distanceF
 
 // Function to create a cube representing a day with text
 function createDayCube(x, y, z, day) {
-    var geometry = new THREE.BoxGeometry(0.3, 0.3, 0.3); // Snížil jsem velikost na 0.6
+    var geometry = new THREE.BoxGeometry(0.3, 0.3, 0.05); // Snížil jsem velikost na 0.6
     var material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
     var cube = new THREE.Mesh(geometry, material);
 
     // Create a separate material for the text
-    var textMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Set your desired text color
+    var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 }); // Set your desired text color
 
     // Create text geometry for the day number
     var textGeometry = new THREE.TextGeometry(day.toString(), {
@@ -107,7 +127,7 @@ function createDayCube(x, y, z, day) {
 
     // Create a mesh with the text geometry and the separate text material
     var textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.set(-0.05, -0.05, 0.15); // Snížil jsem pozici textu vzhledem k geometrii
+    textMesh.position.set(-0.05, -0.05, 0.02); // Snížil jsem pozici textu vzhledem k geometrii
     cube.add(textMesh);
 
     cube.position.set(x, y, z);
@@ -116,23 +136,78 @@ function createDayCube(x, y, z, day) {
 }
 
 // Function to create a monthly calendar
+// Function to create a monthly calendar with day names
+// Function to create a monthly calendar with day names
+// Function to create a monthly calendar with day names
+// Function to create a monthly calendar with day names
 function createMonthlyCalendar(year, month) {
-    var calendar = new THREE.Object3D(); // Container for calendar components
+    var calendar = new THREE.Object3D(); // Kontejner pro komponenty kalendáře
 
-    // Calculate the number of days in the given month
+    // Výpočet počtu dnů v daném měsíci
     var daysInMonth = new Date(year, month + 1, 0).getDate();
 
-    // Create cubes representing each day with text
-    for (var day = 1; day <= daysInMonth; day++) {
-        var row = Math.floor((day - 1) / 7); // Assuming a 7-day week
-        var col = (day - 1) % 7;
+    // Dny v týdnu
+    var daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-        var cube = createDayCube(col * 0.3, -row * 0.3, 0, day); // Snížil jsem vzdálenosti mezi kostkami na 0.8
-        calendar.add(cube);
+    // Zjistění, který den v týdnu začíná měsíc
+    var firstDayOfMonth = new Date(year, month, 0).getDay();
+    console.log("firstDayOfMonth")
+    console.log(firstDayOfMonth)
+
+    // // Přeorganizování pořadí dnů v týdnu, aby pondělí bylo první
+     var rearrangedDaysOfWeek = daysOfWeek.slice(0).concat(daysOfWeek.slice(0, 0));
+
+    // Název měsíce a roku
+    var monthYearText = getMonthName(month) + ' ' + year;
+    var monthYearGeometry = new THREE.TextGeometry(monthYearText, {
+        font: font,
+        size: 0.1,
+        height: 0.03
+    });
+    var monthYearMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+    var monthYearMesh = new THREE.Mesh(monthYearGeometry, monthYearMaterial);
+    monthYearMesh.position.set(0.5, 0.5, 0.05);
+    calendar.add(monthYearMesh);
+
+    // Vytvoření prvního řádku s názvy dní
+    for (var col = 0; col < 7; col++) {
+        var textGeometry = new THREE.TextGeometry(rearrangedDaysOfWeek[col], {
+            font: font,
+            size: 0.08,
+            height: 0.03
+        });
+
+        var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        var textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.set(col * 0.3 - 0.1, 0.3, 0.05);
+        calendar.add(textMesh);
     }
 
+    // Vytvoření druhého řádku s čísly dnů
+    for (var day = 1; day <= daysInMonth; day++) {
+        var row = Math.floor((day - 1 + firstDayOfMonth) / 7); // Předpokládáme 7denní týden
+        var col = (day - 1 + firstDayOfMonth) % 7;
+
+        var cube = createDayCube(col * 0.3, -row * 0.3, 0.07, day);
+        calendar.add(cube);
+    }
     return calendar;
 }
+
+
+
+// Funkce pro získání názvu měsíce
+function getMonthName(month) {
+    var months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return months[month];
+}
+
+
+
+
 
 
 
