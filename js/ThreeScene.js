bcaf1a6cf2d8d767468dd3862ddadb0f7d03b787
Lukáš Brilla <lukas.brilla@student.tuke.sk>
2023-11-26 14:47:24 +0100

    add buttons

--- js/ThreeScene.js
+++ js/ThreeScene.js
@@ -2,13 +2,17 @@ var camera, scene, renderer, controls;
 var group;
 var prismMesh;
 var currentCalendar; // Store a reference to the current calendar
+var isYearly;
 
 // Load the font
 var fontLoader = new THREE.FontLoader();
 var font;
 
 var currentMonth = 10;
-var currentYear = 2023
+var currentYear = 2023;
+
+var circle1;
+var circle2;
 
 // The path should point to the location of helvetiker_regular.typeface.json in your project
 fontLoader.load('./js/font/helvetiker_regular.typeface.json', function (loadedFont) {
@@ -84,7 +88,7 @@ function addObjects() {
     prismMesh.rotation.x = -Math.PI / 2;
     group.add(prismMesh);
 
-    currentCalendar = createMonthlyCalendar(currentYear, currentMonth );
+    currentCalendar = createYearlyCalendar(currentYear);
     positionCalendarOnFace(currentCalendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.33, 0.75, -0.62);
     group.add(currentCalendar);
 
@@ -105,24 +109,59 @@ function positionCalendarOnFace(calendar, prism, rotationY, rotationX, distanceF
 
 
 // Function to create a cube representing a day with text
-function createDayCube(x, y, z, day) {
-    var geometry = new THREE.BoxGeometry(0.3, 0.3, 0.05); // Snížil jsem velikost na 0.6
+function createDayCube(x, y, z, day,month,dayOfWeek) {
+    var geometry = new THREE.BoxGeometry(0.3, 0.3, 0.05);
+    var cubeColor = 0xcccccc;
     var material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
     var cube = new THREE.Mesh(geometry, material);
 
+    if (dayOfWeek === 0 || isHoliday(day, month)) {
+        cubeColor = 0xff0000;
+    }
+
+    var material = new THREE.MeshBasicMaterial({ color: cubeColor });
+    var cube = new THREE.Mesh(geometry, material);
+
+    // Vytvořte samostatný materiál pro text
+    var textColor = (dayOfWeek === 0 || isHoliday(day, month)) ? 0xffffff : 0x000000;
     // Create a separate material for the text
-    var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 }); // Set your desired text color
+    var textMaterial = new THREE.MeshBasicMaterial({ color: textColor });
 
     // Create text geometry for the day number
     var textGeometry = new THREE.TextGeometry(day.toString(), {
-        font: font, // Specify a font (you may need to load a font file)
-        size: 0.1,           // Snížil jsem velikost textu na 0.2
-        height: 0.03          // Snížil jsem tloušťku textu na 0.03
+        font: font,
+        size: 0.1,
+        height: 0.03
     });
 
     // Create a mesh with the text geometry and the separate text material
     var textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.set(-0.05, -0.05, 0.02); // Snížil jsem pozici textu vzhledem k geometrii
+    textMesh.position.set(-0.05, -0.05, 0.02);
+    cube.add(textMesh);
+
+    cube.position.set(x, y, z);
+
+    return cube;
+}
+
+function createDayCubeYearly(x, y, z, day) {
+    var geometry = new THREE.BoxGeometry(0.105, 0.105, 0.05);
+    var material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+    var cube = new THREE.Mesh(geometry, material);
+
+    // Create a separate material for the text
+    var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+
+    // Create text geometry for the day number
+    var textGeometry = new THREE.TextGeometry(day.toString(), {
+        font: font,
+        size: 0.03,
+        height: 0.01
+    });
+
+    // Create a mesh with the text geometry and the separate text material
+    var textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    textMesh.position.set(-0.01, -0.01, 0.02);
     cube.add(textMesh);
 
     cube.position.set(x, y, z);
@@ -131,54 +170,151 @@ function createDayCube(x, y, z, day) {
 }
 
 // Function to create a monthly calendar with day names
-function createMonthlyCalendar(year, month) {
-    var calendar = new THREE.Object3D(); // Kontejner pro komponenty kalendáře
+function createMonthlyCalendar(year, month, isYearlyObject) {
+    var calendar = new THREE.Object3D();
 
     var daysInMonth = new Date(year, month + 1, 0).getDate();
 
     var daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
     var firstDayOfMonth = new Date(year, month, 0).getDay();
-    console.log("firstDayOfMonth")
-    console.log(firstDayOfMonth)
 
-     var rearrangedDaysOfWeek = daysOfWeek.slice(0).concat(daysOfWeek.slice(0, 0));
+    var rearrangedDaysOfWeek = daysOfWeek.slice(0).concat(daysOfWeek.slice(0, 0));
 
-    var monthYearText = getMonthName(month) + ' ' + year;
-    var monthYearGeometry = new THREE.TextGeometry(monthYearText, {
-        font: font,
-        size: 0.1,
-        height: 0.03
-    });
+    var monthYearText;
+    var monthYearGeometry;
     var monthYearMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-    var monthYearMesh = new THREE.Mesh(monthYearGeometry, monthYearMaterial);
-    monthYearMesh.position.set(0.5, 0.5, 0.05);
-    calendar.add(monthYearMesh);
+    var monthYearMesh;
 
-    for (var col = 0; col < 7; col++) {
-        var textGeometry = new THREE.TextGeometry(rearrangedDaysOfWeek[col], {
+    if( isYearlyObject ) {
+        monthYearText = getMonthName(month);
+        monthYearGeometry = new THREE.TextGeometry(monthYearText, {
             font: font,
-            size: 0.08,
+            size: 0.06,
+            height: 0.015
+        });
+        monthYearMesh = new THREE.Mesh(monthYearGeometry, monthYearMaterial);
+        monthYearMesh.position.set(0.2, 0.45, 0.08);
+    }
+    else {
+        monthYearText = getMonthName(month) + ' ' + year;
+        monthYearGeometry = new THREE.TextGeometry(monthYearText, {
+            font: font,
+            size: 0.1,
             height: 0.03
         });
+        monthYearMesh = new THREE.Mesh(monthYearGeometry, monthYearMaterial);
+        monthYearMesh.position.set(0.5, 0.5, 0.05);
+    }
 
-        var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-        var textMesh = new THREE.Mesh(textGeometry, textMaterial);
-        textMesh.position.set(col * 0.3 - 0.1, 0.3, 0.05);
-        calendar.add(textMesh);
+    calendar.add(monthYearMesh);
+
+
+    for (var col = 0; col < 7; col++) {
+        if( isYearlyObject ) {
+            var textGeometry = new THREE.TextGeometry(rearrangedDaysOfWeek[col], {
+                font: font,
+                size: 0.03,
+                height: 0.01
+            });
+
+            var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+            var textMesh = new THREE.Mesh(textGeometry, textMaterial);
+            textMesh.position.set(col * 0.11, 0.3, 0.08);
+            calendar.add(textMesh);
+        }
+        else {
+            var textGeometry = new THREE.TextGeometry(rearrangedDaysOfWeek[col], {
+                font: font,
+                size: 0.08,
+                height: 0.03
+            });
+
+            var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+            var textMesh = new THREE.Mesh(textGeometry, textMaterial);
+            textMesh.position.set(col * 0.3 - 0.1, 0.3, 0.05);
+            calendar.add(textMesh);
+        }
+
     }
 
     for (var day = 1; day <= daysInMonth; day++) {
         var row = Math.floor((day - 1 + firstDayOfMonth) / 7);
         var col = (day - 1 + firstDayOfMonth) % 7;
-
-        var cube = createDayCube(col * 0.3, -row * 0.3, 0.07, day);
+        var cube;
+        var yearlyShiftX = 0.06
+        var yearlyShiftY = 0.15
+        if( isYearlyObject ) {
+            cube = createDayCubeYearly(yearlyShiftX + (col * 0.105), yearlyShiftY + (-row * 0.105), 0.11, day);
+        }
+        else {
+            cube = createDayCube(col * 0.3, -row * 0.3, 0.07, day,month,(day + firstDayOfMonth) % 7);
+        }
         calendar.add(cube);
     }
 
-    createClickableCircle(1);
-    createClickableCircle(0);
+    if(!isYearlyObject){
+        circle1 = createClickableCircle(1,isYearlyObject);
+        circle2 = createClickableCircle(0,isYearlyObject);
+    }
     return calendar;
+}
+
+function isHoliday(day, month) {
+    const holidays = {
+        0: [1, 6],
+        4: [1, 8],
+        6: [5],
+        7: [29],
+        8: [1, 15],
+        10: [1, 17],
+        11: [24, 25, 26]
+    };
+
+    return holidays[month] && holidays[month].includes(day);
+}
+
+// Function to create a monthly calendar with day names
+// Function to create a yearly calendar with monthly calendars
+function createYearlyCalendar(year) {
+    var yearlyCalendar = new THREE.Object3D();
+
+    var calendarsInRow = 4;
+    var calendarSpacing = 0.2; // Adjust the spacing between calendars as needed
+
+    for (var month = 0; month < 12; month++) {
+        var calendar = createMonthlyCalendar(year, month, true);
+
+        // Adjust the scale of each monthly calendar
+        calendar.scale.set(0.5, 0.5, 0.5); // Adjust the scale factor as needed
+
+        // Calculate the row and column for each calendar
+        var row = Math.floor(month / calendarsInRow);
+        var col = month % calendarsInRow;
+
+        // Adjust the position to arrange calendars in a grid
+        calendar.position.set(col * (calendarSpacing + 0.25), -row * (calendarSpacing + 0.4), 0);
+
+        yearlyCalendar.add(calendar);
+    }
+
+    var yearText = year.toString();
+    var yearGeometry;
+    var yearMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+    var yearMesh;
+
+    yearGeometry = new THREE.TextGeometry(yearText, {
+        font: font,
+        size: 0.06,
+        height: 0.015
+    });
+    yearMesh = new THREE.Mesh(yearGeometry, yearMaterial);
+    yearMesh.position.set(0.75, 0.45, 0.04);
+    yearlyCalendar.add(yearMesh);
+    circle1 = createClickableCircle(1,true);
+    circle2 = createClickableCircle(0,true);
+
+    return yearlyCalendar;
 }
 
 function getMonthName(month) {
@@ -233,23 +369,31 @@ window.addEventListener('mousemove', function (event) {
     }
 });
 
-function createClickableCircle(direction) {
+function createClickableCircle(direction,isYearly) {
     // Create a parent container
     var container = new THREE.Object3D();
 
-    var circleGeometry = new THREE.CircleGeometry(0.5, 32);
-    var circleMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
+    var circleGeometry = new THREE.CircleGeometry(0.10, 32);
+    var circleMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc, side: THREE.DoubleSide });
     var circleMesh = new THREE.Mesh(circleGeometry, circleMaterial);
     // Rotate the circle by 90 degrees
-    circleMesh.rotation.set(0, Math.PI / 2, 0);
+
+    circleMesh.rotation.set(Math.PI / 2, -Math.PI / 3, 0);
+
+
 
     // Set up event listeners for each ball
     circleMesh.userData = {
         isClickEnabled: true,
         onClick: function () {
-            console.log("Ball Clicked!");
-            if (direction === 1) changeMonth(1);
-            else if (direction === 0) changeMonth(-1);
+            if(isYearly){
+                if (direction === 1) changeYear(1);
+                else if (direction === 0) changeYear(-1);
+            }
+            else{
+                if (direction === 1) changeMonth(1);
+                else if (direction === 0) changeMonth(-1);
+            }
         }
     };
 
@@ -263,25 +407,94 @@ function createClickableCircle(direction) {
     container.add(circleMesh);
 
     // Create an arrow helper (pointing to the right in this case)
-    var arrowDirection = new THREE.Vector3(1, 0, 0);
-    var arrowLength = 1;
-    var arrowHelper = new THREE.ArrowHelper(arrowDirection, new THREE.Vector3(0, 0, 0), arrowLength * 0.5, 0xff0000);
-    // Rotate the arrow helper by 90 degrees to match the circle
+    function createArrowMesh(direction) {
+        // Vytvořte materiál pro šipku s texturou
+        var arrowMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide });
 
-    if (direction === 1) arrowHelper.rotation.set(-Math.PI / 2, 0, 0);
-    else if (direction === 0) arrowHelper.rotation.set(Math.PI / 2, 0, 0);
+        // Vytvořte textovou geometrii pro šipku
+        var arrowGeometry = new THREE.TextGeometry(direction, {
+            font: font,
+            size: 0.1,
+            height: 0.0
+        });
 
-    container.add(arrowHelper);
+        // Vytvořte mesh pro šipku s použitím textové geometrie a materiálu
+        var arrowMesh = new THREE.Mesh(arrowGeometry, arrowMaterial);
+
+        return arrowMesh;
+    }
+
+// Vytvořte šipky
+    var leftArrow = createArrowMesh("<");
+    var rightArrow = createArrowMesh(">");
+
+// Nastavte rotaci a pozici pro šipky
+    leftArrow.rotation.set(Math.PI / 2, -Math.PI / 3, Math.PI / 2); // Přizpůsobte hodnoty podle potřeby
+    leftArrow.position.set(0.67, 0.47, -1.35); // Přizpůsobte hodnoty podle potřeby
+
+    rightArrow.rotation.set(Math.PI / 2, -Math.PI / 3, Math.PI / 2); // Přizpůsobte hodnoty podle potřeby
+    rightArrow.position.set(0.67, 0.47, 1.27); // Přizpůsobte hodnoty podle potřeby
+
+// Přidejte meshy do scény
+    scene.add(leftArrow);
+    scene.add(rightArrow);
 
     // Move the container to a new position
 
-    if (direction === 1) container.position.set(0, 0.5, -2.5);
-    else if (direction === 0) container.position.set(0, 0.5, 2.5);
+    if (direction === 1) container.position.set(0.65, 0.5, -1.3);
+    else if (direction === 0) container.position.set(0.65, 0.5, 1.3);
 
     scene.add(container);
 
     // Add the ball to the array
     balls.push(circleMesh);
+    return container;
+}
+
+// Create a button element
+var toggleViewButton = document.createElement('button');
+toggleViewButton.textContent = 'Toggle View';
+toggleViewButton.style.position = 'absolute';
+toggleViewButton.style.top = '10px';
+toggleViewButton.style.left = '10px';
+
+// Append the button to the document body
+document.body.appendChild(toggleViewButton);
+
+// Register a click event listener for the button
+var debounceTimeout;
+toggleViewButton.addEventListener('click', function () {
+    if (!debounceTimeout) {
+        toggleCalendarView();
+        debounceTimeout = setTimeout(function () {
+            debounceTimeout = null;
+        }, 1000); // Set the timeout duration (in milliseconds)
+    }
+});
+
+// Initialize isYearly property for the initial calendar
+isYearly = false;
+
+// Function to toggle between monthly and yearly views
+
+function toggleCalendarView() {
+    // Remove the existing calendar from the group
+    group.remove(currentCalendar);
+    balls = [];
+    scene.remove(circle1);
+    scene.remove(circle2);
+
+    if (!isYearly) {
+        // Toggle to monthly view
+        currentCalendar = createMonthlyCalendar(currentYear, currentMonth, false);
+        positionCalendarOnFace(currentCalendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.33, 0.75, -0.62);
+    } else {
+        // Toggle to yearly view
+        currentCalendar = createYearlyCalendar(currentYear);
+        positionCalendarOnFace(currentCalendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.33, 0.75, -0.62);
+    }
+    isYearly = !isYearly;
+    group.add(currentCalendar); // Add the new calendar to the group
 }
 
 
@@ -300,7 +513,20 @@ function changeMonth(delta) {
     group.remove(currentCalendar);
 
     // Create and add the new calendar for the updated month
-    var newCalendar = createMonthlyCalendar(currentYear, currentMonth);
+    var newCalendar = createMonthlyCalendar(currentYear, currentMonth, false);
+    positionCalendarOnFace(newCalendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.33, 0.75, -0.62);
+    newCalendar.name = "calendar_" + currentMonth;
+    group.add(newCalendar); // Add the new calendar to the group
+    currentCalendar = newCalendar; // Update the reference to the current calendar
+}
+
+function changeYear(delta) {
+    currentYear += delta;
+    // Remove the existing calendar from the group
+    // group.remove(scene.getObjectByName("calendar_" + currentMonth));
+    group.remove(currentCalendar);
+    // Create and add the new calendar for the updated month
+    var newCalendar = createYearlyCalendar(currentYear);
     positionCalendarOnFace(newCalendar, prismMesh, Math.PI / 2, -Math.PI / 6, 0.33, 0.75, -0.62);
     newCalendar.name = "calendar_" + currentMonth;
     group.add(newCalendar); // Add the new calendar to the group
